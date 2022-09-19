3cf85b0a7cc24c438339ffe702a5f1d2af3f7d9b
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <phtfao@gmail.com>
2022-09-19 08:01:27 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 cp .env.example .env
 composer install
 touch database/database.sqlite
+chmod 777 database/database.sqlite
+chmod 777 storage -R
+
 cp -R vendor/phtfao/panako/tests .
 php /panako/artisan migrate:refresh --seed
